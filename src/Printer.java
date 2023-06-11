cc4495ce35cb8cdc69565184431d4695d85a2289
Orest Klymko <116631168+OrestKlymko@users.noreply.github.com>
2023-06-11 15:28:13 +0200

    Update Printer.java

--- src/Printer.java
+++ src/Printer.java
@@ -2,7 +2,7 @@ import Shapes.abstractClass.Shape;
 
 public class Printer {
 
-	public void PrintInfo(Shape shape){
+	public void printInfo(Shape shape){
 		System.out.println(shape.getNameOfShape());
 	}
 }
